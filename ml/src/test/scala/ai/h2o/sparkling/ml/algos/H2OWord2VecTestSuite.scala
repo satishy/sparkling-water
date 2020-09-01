ef532ed5729421875cc1eb85b5ea347be92ab35e
Jakub Háva <jakub.hava@gmail.com>
2020-09-01 12:30:47 +0200

    finish test

--- ml/src/test/scala/ai/h2o/sparkling/ml/algos/H2OWord2VecTestSuite.scala
+++ ml/src/test/scala/ai/h2o/sparkling/ml/algos/H2OWord2VecTestSuite.scala
@@ -73,6 +73,9 @@ class H2OWord2VecTestSuite extends FunSuite with Matchers with SharedH2OTestCont
   test("Basic Word2Vec") {
     val model = getPipeline().fit(trainingDataset)
     val result = model.transform(testingDataset)
+    val actual = result.select("prediction").map(row => row.getString(0)).take(10)
+    val expected = Array("accounting", "accounting", "administrative", "accounting", "accounting", "accounting", "administrative", "accounting", "accounting", "accounting")
+    assert(actual.sameElements(expected))
   }
 
   test("Word2Vec with pre-trained model") {}
